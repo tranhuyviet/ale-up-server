e9b7e0d1742faf69cc8b81e257fd753a1b22b162
tranhuyviet <44466617+tranhuyviet@users.noreply.github.com>
2021-03-27 17:09:40 +0200

    handle query with both discount and price

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,16 +1,35 @@
 import Product from '../../models/productModel.js';
 import Market from '../../models/marketModel.js';
 
-function queryName(name) {
-    const splitName = name.split(' ');
+function queryName(name, discount, price) {
     let rtName = [];
-    for (const name of splitName) {
-        rtName.push({ name: { $regex: `.*${name}.*`, $options: 'i' } });
+    let returnObj = {};
+    if (name) {
+        const splitName = name.split(' ');
+        for (const name of splitName) {
+            rtName.push({ name: { $regex: `.*${name}.*`, $options: 'i' } });
+        }
     }
 
-    return {
-        $and: rtName,
-    };
+    if (discount.length > 0) {
+        rtName.push({ discount: { $gte: discount[0] * 1 } });
+        rtName.push({ discount: { $lte: discount[1] * 1 } });
+    }
+
+    if (price.length > 0) {
+        rtName.push({ newPrice: { $gte: price[0] * 1 } });
+        rtName.push({ newPrice: { $lte: price[1] * 1 } });
+    }
+
+    if (rtName.length > 0) {
+        returnObj = {
+            $and: rtName,
+        };
+    }
+
+    console.log('return obj', returnObj);
+
+    return returnObj;
 }
 
 export default {
@@ -19,12 +38,12 @@ export default {
     // },
     Query: {
         // Get all products
-        products: async (_, { name, market, discount, offset = 0, limit = 20, sort = 'discount' }) => {
+        products: async (_, { name, market, discount, price, offset = 0, limit = 20, sort = 'discount' }) => {
             try {
                 let query = {};
-                if (name) {
-                    query = queryName(name);
-                }
+                // if (name) {
+                query = queryName(name, discount, price);
+                // }
 
                 if (market && market !== 'all') {
                     const checkMarket = await Market.find({ name: market });
@@ -34,11 +53,29 @@ export default {
                     query = { ...query, market: checkMarket[0].id };
                 }
 
-                if (discount.length > 0) {
-                    query = { ...query, $and: [{ discount: { $gte: discount[0] * 1 } }, { discount: { $lte: discount[1] * 1 } }] };
-                }
+                // if (discount.length === 0) discount = [0, 100];
+                // if (price.length === 0) price = [0, 5000];
 
-                console.log('Variables:', name, market, discount);
+                // query = {
+                //     ...query,
+                //     $and: [
+                //         { discount: { $gte: discount[0] * 1 } },
+                //         { discount: { $lte: discount[1] * 1 } },
+                //         { newPrice: { $gte: price[0] * 1 } },
+                //         { newPrice: { $lte: price[1] * 1 } },
+                //     ],
+                // };
+
+                // if (discount.length > 0) {
+                //     query = { ...query, $and: [{ discount: { $gte: discount[0] * 1 } }, { discount: { $lte: discount[1] * 1 } }] };
+                // }
+
+                // if (price.length > 0) {
+                //     query = { ...query, $and: [{ newPrice: { $gte: price[0] * 1 } }, { newPrice: { $lte: price[1] * 1 } }] };
+                // }
+
+                console.log(query);
+                // console.log('Variables:', name, market, discount, price);
 
                 let sortDB = {};
                 if (sort === 'nameAO') {
@@ -57,6 +94,7 @@ export default {
 
                 // console.log(sortDB);
                 // const products = await Product.find({ $or: [{ name: { $regex: '.*naudan.*' } }] })
+
                 const total = await Product.find(query).countDocuments();
                 const products = await Product.find(query)
                     .sort(sortDB)
@@ -65,6 +103,10 @@ export default {
                     })
                     .limit(limit)
                     .skip(offset);
+
+                // const productsGetMinMaxPrice = await Product.find().sort({ newPrice: 1 });
+                // const total = productsGetMinMaxPrice.length;
+                // console.log('CAC', productsGetMinMaxPrice[0].newPrice, productsGetMinMaxPrice[productsGetMinMaxPrice.length - 1].newPrice);
 
                 if (!products) {
                     throw new Error('Can not get products');
@@ -118,6 +160,34 @@ export default {
                 }
                 // console.log(products);
                 return products;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        // Get product min and max price (for filter price)
+        productPrice: async () => {
+            try {
+                const product = await Product.aggregate([
+                    {
+                        $group: {
+                            _id: '$id',
+                            min: { $min: '$newPrice' },
+                            max: { $max: '$newPrice' },
+                        },
+                    },
+                ]);
+                console.log(product[0]);
+                return {
+                    min: product[0].min,
+                    max: product[0].max,
+                };
+                // const products = await Product.find().sort({ newPrice: 1 });
+                // const productPrice = {
+                //     min: products[0].newPrice,
+                //     max: products[products.length - 1].newPrice,
+                // };
+                // return productPrice;
             } catch (error) {
                 console.log(error);
             }
--- graphql/typeDefs/index.js
+++ graphql/typeDefs/index.js
@@ -24,12 +24,20 @@ export default gql`
         products: [Product]!
     }
 
+    type ProductPrice {
+        min: Float!
+        max: Float!
+    }
+
     type Query {
         "Get all products"
-        products(name: String, market: String, discount: [Int!], offset: Int, limit: Int, sort: String): ReturnProducts!
+        products(name: String, market: String, discount: [Int!], price: [Int!], offset: Int, limit: Int, sort: String): ReturnProducts!
 
         "Get products popular to introduce in hompage"
         productIntroduce: [Product]!
+
+        "Get product min price and max price"
+        productPrice: ProductPrice!
 
         "Get all markets"
         markets: [Market]!
